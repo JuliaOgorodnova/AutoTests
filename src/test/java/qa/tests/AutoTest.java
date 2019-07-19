164b5923be8e4d14976c2cb81a3ad9c2d4340627
Julia <julia.ogorodnova@motorsport.com>
2019-07-19 18:05:26 +0500

    +edits

--- src/test/java/qa/tests/AutoTest.java
+++ src/test/java/qa/tests/AutoTest.java
@@ -15,19 +15,19 @@ class AutoTest extends TestConfig {
 	@DisplayName("Проверка видимости элементов в таблице")
 	void testVisibilityContactInfo() {
 		CELL_LIST_TITLE.shouldBe(Condition.visible);
-		CONTACT_INFO_TABLE.waitUntil(Condition.visible, 10000);
-		FIRST_NAME.waitUntil(Condition.visible, 10000);
-		LAST_NAME.waitUntil(Condition.visible, 10000);
-		BIRTHDAY.waitUntil(Condition.visible, 10000);
-		ADDRESS.waitUntil(Condition.visible, 10000);
-		UPDATE_CONTACT.waitUntil(Condition.visible, 10000);
+		CONTACT_INFO_TABLE.shouldBe(Condition.visible);
+		FIRST_NAME.shouldBe(Condition.visible);
+		LAST_NAME.shouldBe(Condition.visible);
+		BIRTHDAY.shouldBe(Condition.visible);
+		ADDRESS.shouldBe(Condition.visible);
+		UPDATE_CONTACT.shouldBe(Condition.visible);
 	}
 
 	@Test
 	@DisplayName("Создание нового пользователя")
 	void testCreateNewUser() {
-		CELL_LIST_TITLE.waitUntil(Condition.visible, 10000);
-		CONTACT_INFO_TABLE.waitUntil(Condition.visible, 10000);
+		CELL_LIST_TITLE.shouldBe(Condition.visible);
+		CONTACT_INFO_TABLE.shouldBe(Condition.visible);
 		fillContactInfo(ContactInfo.buildDefaultContactInfo());
 		CATEGORY.get(0).click();
 		LAST_NAME.click();
@@ -37,8 +37,8 @@ class AutoTest extends TestConfig {
 	@Test
 	@DisplayName("Редактирование существующего пользователя")
 	void testEditingUser() {
-		CELL_LIST_TITLE.waitUntil(Condition.visible, 10000);
-		CONTACT_INFO_TABLE.waitUntil(Condition.visible, 10000);
+		CELL_LIST_TITLE.shouldBe(Condition.visible);
+		CONTACT_INFO_TABLE.shouldBe(Condition.visible);
 		EDITING_USER.click();
 		fillContactInfo(ContactInfo.buildDefaultContactInfo());
 		LAST_NAME.click();
@@ -48,8 +48,8 @@ class AutoTest extends TestConfig {
 	@Test
 	@DisplayName("Редактирование адреса у существующего пользователя")
 	void testEditingAddressUser() {
-		CELL_LIST_TITLE.waitUntil(Condition.visible, 10000);
-		CONTACT_INFO_TABLE.waitUntil(Condition.visible, 10000);
+		CELL_LIST_TITLE.shouldBe(Condition.visible);
+		CONTACT_INFO_TABLE.shouldBe(Condition.visible);
 		EDITING_USER.click();
 		ADDRESS.setValue("1");
 		UPDATE_CONTACT.click();
